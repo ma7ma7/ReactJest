392809da36e33225beeb3b9a5ee0502b05e25096
Amane Mahmoud <mahmoudamane5@gmail.com>
2021-01-29 23:15:59 +0100

    Add CommentBox Logic

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CommentBox = () => {
-  return <div>Comment Box</div>;
-};
+class CommentBox extends Component {
+  state = { comment: '' };
+
+  handleChange = (e) => {
+    this.setState({ comment: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({ comment: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <textarea
+          value={this.state.comment}
+          onChange={this.handleChange}
+        ></textarea>
+        <button>Add Comment</button>
+      </form>
+    );
+  }
+}
 
 export default CommentBox;
