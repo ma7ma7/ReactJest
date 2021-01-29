440ff1d7604548bf8e09d5a00afe54a0c04b3087
Amane Mahmoud <mahmoudamane5@gmail.com>
2021-01-29 16:45:20 +0100

    Add Enzyme and implement it with App COmponent

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentsList from 'components/CommentsList';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it('Is App Render CommentBox', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  expect(div.innerHTML).toContain('Comment Box');
-  ReactDOM.unmountComponentAtNode(div);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('Is App render CommentsList', () => {
+  expect(wrapped.find(CommentsList).length).toEqual(1);
 });
